c213a1f5f4339a50207d361f537f0f7dd1df8eda
Leonid349 <159018167+Leonid349@users.noreply.github.com>
2024-03-11 15:13:37 +0300

    Add files via upload

--- easy.h
+++ easy.h
@@ -4,4 +4,14 @@
 void itc_name();
 void itc_fio();
 int itc_abc(int num);
+int itc_revnbr(int num);
+bool itc_iseven(int num);
+int itc_max(int num1, int num2);
+int itc_min(int num1, int num2);
+double itc_fmax(double num, double num2);
+double itc_fmin(double num, double num2);
+int itc_sqrt(int num);
+int itc_skv(int num);
+int itc_spr(int a, int b);
+
 #endif // EASY_H_INCLUDED
